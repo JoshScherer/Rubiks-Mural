5262e6476d34b06ebb9700404d19ac8d8bdedf4f
JoshScherer <89754318+JoshScherer@users.noreply.github.com>
2023-08-18 16:41:58 -0500

    Relative import for cube into solver

--- solver/basic-solver.py
+++ solver/basic-solver.py
@@ -1,5 +1,23 @@
+import sys
+import os
+
+# Ugly code to deal with relative imports across directories
+sys.path.insert(0, str(os.getcwd()) + '/cube')
+
+from cube import Cube
+
 def main():
-    print("Hello world")
+    print("Entering main function")
+    test_cube = Cube()
+
+    test_cube.initialize_cube()
+    print("BEFORE:", end=" ")
+    test_cube.visualize_cube()
+
+    test_cube.rotate_cube_along_y_counterclockwise()
+
+    print("AFTER:", end="  ")
+    test_cube.visualize_cube()
 
 
 if __name__ == '__main__':
