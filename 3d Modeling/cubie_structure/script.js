01a6cf0c1143e659a88e10de0775bea5b7220566
JoshScherer <89754318+JoshScherer@users.noreply.github.com>
2023-11-01 13:19:15 -0500

    Function for 3d cubie

--- 3d Modeling/cubie_structure/script.js
+++ 3d Modeling/cubie_structure/script.js
@@ -4,19 +4,17 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 var scene = new THREE.Scene();
 scene.background = new THREE.Color('gainsboro');
 
-var camera = new THREE.PerspectiveCamera(30, innerWidth / innerHeight);
+var camera = new THREE.PerspectiveCamera(30, innerWidth / innerHeight, 0.1, 1000);
 camera.position.set(0, 0, 10);
 camera.lookAt(scene.position);
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(innerWidth, innerHeight);
-renderer.setAnimationLoop(animationLoop);
 document.body.appendChild(renderer.domElement);
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+var controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 controls.dampingFactor = 0.25;
-
 
 window.addEventListener("resize", () => {
     camera.aspect = innerWidth / innerHeight;
@@ -31,33 +29,33 @@ var light = new THREE.DirectionalLight('white', 0.5);
 light.position.set(1, 1, 1);
 scene.add(light);
 
-var canvas = document.createElement('CANVAS');
-canvas.width = 512;
-canvas.height = 512;
+function createColoredCube(color_top, color_bot, color_left, color_right, color_front, color_back, center) {
+    var canvas = document.createElement('canvas');
+    canvas.width = 512;
+    canvas.height = 512;
 
-var context = canvas.getContext('2d');
-context.fillStyle = 'tan';
-context.fillRect(0, 0, 512, 512);
-context.strokeStyle = 'black';
-context.lineWidth = 32;
-context.strokeRect(16, 16, 512 - 32, 512 - 32);
+    var context = canvas.getContext('2d');
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, 512, 512);
+    context.strokeStyle = 'black';
+    context.lineWidth = 32;
+    context.strokeRect(16, 16, 512 - 32, 512 - 32);
 
-// Array of colors for each face
-var colors = [
-    'red', 'green', 'blue', 'yellow', 'magenta', 'cyan'
-];
+    var colors = [color_right, color_left, color_top, color_bot, color_front, color_back];
+    var materials = colors.map(color => new THREE.MeshBasicMaterial({ color: color, map: new THREE.CanvasTexture(canvas) }));
 
-// Create an array of materials with a different color for each face
-var materials = colors.map(color => new THREE.MeshLambertMaterial({ color: color, map: new THREE.CanvasTexture(canvas) }));
+    var cube = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), materials);
+    cube.position.set(center.x, center.y, center.z);
 
-var object = new THREE.Mesh(
-    new THREE.BoxGeometry(2, 2, 2),
-    materials
-);
-scene.add(object);
+    return cube;
+}
+
+var cube = createColoredCube('white', 'yellow', 'green', 'blue', 'red', 'orange', new THREE.Vector3(0, 0, 0));
+scene.add(cube);
 
 function animationLoop() {
-    controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+    controls.update();
     renderer.render(scene, camera);
 }
 
+renderer.setAnimationLoop(animationLoop);
